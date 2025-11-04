ff10442968dc3c9833c6b3a1fb48d9e8010be769
Sam Martin <broccoliboy@gmail.com>
2025-11-03 20:43:57 -0600

    remove deprecated pkg_resources and replace with importlib approach

--- vnoise/__init__.py
+++ vnoise/__init__.py
@@ -2,9 +2,9 @@ from .vnoise import Noise
 
 
 def _get_version() -> str:
-    import pkg_resources
+    from importlib.metadata import version
 
-    return pkg_resources.get_distribution("vnoise").version
+    return version(__package__ or __name__)
 
 
 __version__ = _get_version()
